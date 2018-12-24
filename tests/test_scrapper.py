8b0c9d87880983feb470591ec9edc7e7f0da28be
Medi Assumani <mediassumani49@gmail.com>
2018-12-23 19:08:14 -0500

    [Add] starter code for testing

--- tests/test_scrapper.py
+++ tests/test_scrapper.py
@@ -1,2 +1,20 @@
-
+import unitest
 import Scrapper
+
+class TestEscrapper(unitest.TestCase):
+
+    def test_init(self):
+        pass
+
+    def test_raw_data(self):
+        pass
+
+    def test_clean_data(self):
+        pass
+
+    def test_write_in_file(self):
+        pass
+
+
+if __name__ == "__main__":
+    unitest.main()
